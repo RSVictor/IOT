8aaafdd28aa44c6221741e1591d027d73b02c62d
RSVictor <152220391+RSVictor@users.noreply.github.com>
2025-02-27 20:02:41 -0300

    update not found

--- DOMOTICA/domotica/src/views/Management.vue
+++ DOMOTICA/domotica/src/views/Management.vue
@@ -5,7 +5,7 @@
 
 <template>
     <h1>Management page!</h1>
-    <img src="https://th.bing.com/th/id/OIP.G5adnm-vrYYWBOwoYoTHUQAAAA?w=204&h=204&c=7&r=0&o=5&pid=1.7">
+    
 
 
 </template>
--- DOMOTICA/domotica/src/views/NotFound.vue
+++ DOMOTICA/domotica/src/views/NotFound.vue
@@ -5,6 +5,7 @@
 
 <template>
     <h1>Page not found ☹</h1>
+    <img src="https://th.bing.com/th/id/OIP.G5adnm-vrYYWBOwoYoTHUQAAAA?w=204&h=204&c=7&r=0&o=5&pid=1.7">
     
 </template>
 
