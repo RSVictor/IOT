4f1a1f0a2a423be9c6ae77f452e22ab9a741ff40
Victor <victorribeiro304@hotmail.com>
2025-04-10 22:19:30 -0300

    Upadate

--- DOMOTICA/domotica/src/services/devices.service.ts
+++ DOMOTICA/domotica/src/services/devices.service.ts
@@ -1,6 +1,12 @@
-import type { ApiResponse, Device, Environment } from "@/models/devices";
+import type { ApiResponse, Device, Environment, NewDevice } from "@/models/devices";
 import { getCdn, getApi } from "./services.config";
 
 export const getDevices = (): Promise<ApiResponse<Device>>=> {    
     return getCdn().get("?content_type=device");
 }
+
+export const saveDevice = (device:NewDevice): Promise<void>=> {    
+    return getCdn().post("/", device);
+}
+
+
