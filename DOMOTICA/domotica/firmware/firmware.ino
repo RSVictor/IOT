c5c4a55f9f5b436301dcaa73f99a654b252a5f12
RSVictor <152220391+RSVictor@users.noreply.github.com>
2025-05-08 21:56:31 -0300

    update

--- DOMOTICA/domotica/firmware/firmware.ino
+++ DOMOTICA/domotica/firmware/firmware.ino
@@ -5,66 +5,61 @@
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
 
-const char* contentfulURL = "https://cdn.contentful.com/spaces/9emcot1k760p/environments/master/entries?content_type=device&select=fields.pin,fields.state";
-const char* accessToken = "Bearer wm8wcDePYAfU6Ub5zOIj6DQlsq4SYyfBtuS1Cxl5Iyk";
+const char* contentfulURL = "https://cdn.contentful.com/spaces/8qlw091a3qx6/environments/master/entries?content_type=device&select=fields.pin,fields.state";
+const char* accessToken = "Bearer WsXw2sk4bxoT34IdCt7Mv_Vm1gI2p8rmt443PhZWOT8"
 
 unsigned long timerScan = 0;
-#define SCAN_DELAY 3000
-
+#define SCAN_DELAY 3000 
 void setup()
 {
     Serial.begin(115200);
-    WiFi.begin(ssid,password);
+    WiFI.begin(ssid,passsword);
     Serial.println("Starting Wifi Connection!");
 
-    while(WiFi.status() != WL_CONNECTED){
+    while(WiFI.status() != WL_CONNECTED){
         delay(100);
         Serial.println("*");
     }
-    Serial.printf("\n Successfully Connected with: %s", ssid);    
+    Serial.printf("\n Successfully Conected with: %s", ssid);
 }
 
-void loop()
+void void loop()
 {
-    if( (millis() - timerScan) >= SCAN_DELAY ){
-        Serial.println("\n **** SCAN STARTED *** \n");
-        timerScan = millis();
-        scanRoutine();
-        Serial.println("\n **** SCAN ENDED *** \n");
-    }    
+ if( (millis() - timerScan) >= SCAN_DELAY){
+    Serial.println("\n *** SCAN STARTED *** \n");
+    timerScan = millis();
+    scanRoutine();
+    Serial.println("\n *** SCAN ENDED *** \n");
+ }   
 }
 
 void scanRoutine(){
-
     HTTPClient http;
     http.begin(contentfulURL);
-    http.addHeader("Authorization", accessToken);
+    http.addheader("Authorization", accessToken);
 
-    int httpResponseStatus = http.GET();
+   int httpResponseStatus =  http.GET()
 
-    if(httpResponseStatus > 0){
+   if(httpResponseStatus > 0){
         String response = http.getString();
 
-        const int responseSize = response.length() * 1.1;        
+        const int responseSize = res.length() * 1.1;
         DynamicJsonDocument json(responseSize);
-        deserializeJson(json,response);
+        deserializejson(json,response);
 
-        JsonArray items = json["items"];
+        jsonArray item = json["items"];
 
-        for( JsonObject item : items ){
+        for(JsonObject item : items ){
             int pin = item["fields"]["pin"] | -1;
-            bool state = item["fields"]["state"] | false;
+            bool state item["fields"]["state"] / false;
 
-            if(pin > 0 ){
+            if(pin > 0){
                 pinMode(pin, OUTPUT);
                 digitalWrite(pin, state);
-                Serial.printf("\n PIN: %d, STATE: %s", pin, state? "ON":"OFF");                
-            }
+                Serial.printf("PIN: %d, ", pin, state? "ON":"OFF");            }
         }
-
-    }
-    else{
-        Serial.printf("\n Error when calling backend: %d \n",httpResponseStatus);
-    }
-
+   }
+   else{
+    Serial.printf("\n Error when calling backend: %d \n", httpResponseStatus);
+   }
 }
